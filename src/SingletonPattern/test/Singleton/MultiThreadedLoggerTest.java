f41440806efd3b97175ce9cb057537d7220edfb3
Falcon-09 <nipunairr1@gmail.com>
2025-02-22 01:32:57 +0530

    Singleton Pattern

--- src/SingletonPattern/test/Singleton/MultiThreadedLoggerTest.java
+++ src/SingletonPattern/test/Singleton/MultiThreadedLoggerTest.java
@@ -3,6 +3,9 @@ package SingletonPattern.test.Singleton;
 import SingletonPattern.Logger;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -14,14 +17,13 @@ public class MultiThreadedLoggerTest {
     public void testMultiThreadedAccess() throws InterruptedException {
         final int numThreads = 10;
         ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+        Set<Logger> instanceSet = Collections.synchronizedSet(new HashSet<>());
 
         // Create a Runnable task that accesses the Logger instance
         Runnable task = () -> {
             Logger logger = Logger.getInstance();
+            instanceSet.add(logger); // Store instance
             logger.debug("Debug log from thread: " + Thread.currentThread().getName());
-            logger.info("Info log from thread: " + Thread.currentThread().getName());
-            logger.warn("Warn log from thread: " + Thread.currentThread().getName());
-            logger.error("Error log from thread: " + Thread.currentThread().getName());
         };
 
         // Submit tasks to the executor
@@ -34,8 +36,6 @@ public class MultiThreadedLoggerTest {
         executorService.awaitTermination(1, TimeUnit.MINUTES);
 
         // Verify that only one instance of Logger was created
-        Logger logger1 = Logger.getInstance();
-        Logger logger2 = Logger.getInstance();
-        assertSame(logger1, logger2, "Both instances should be the same.");
+        assertEquals(1, instanceSet.size(), "All threads should get the same Logger instance.");
     }
 }
